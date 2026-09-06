73cb3787ce6bb3d500c871e1e3aa9d6dbeda2f70
Sardor <albakiev.sardorbek@gmail.com>
2026-09-07 02:19:08 +0500

    fix(storage): graceful import of MySQLReadOnlyClient if pymysql not installed

--- src/storage/__init__.py
+++ src/storage/__init__.py
@@ -1,5 +1,8 @@
 from .sqlite_events_repo import SQLiteEventsRepo
-from .mysql_readonly_client import MySQLReadOnlyClient, ReadOnlySecurityViolation, ReadOnlyQueryTimeoutError
 
-__all__ = ["SQLiteEventsRepo", "MySQLReadOnlyClient", "ReadOnlySecurityViolation", "ReadOnlyQueryTimeoutError"]
+try:
+    from .mysql_readonly_client import MySQLReadOnlyClient, ReadOnlySecurityViolation, ReadOnlyQueryTimeoutError
+    __all__ = ["SQLiteEventsRepo", "MySQLReadOnlyClient", "ReadOnlySecurityViolation", "ReadOnlyQueryTimeoutError"]
+except ImportError:
+    __all__ = ["SQLiteEventsRepo"]
 
